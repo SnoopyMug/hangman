4bbe19387c891ff69a9fc3b0d59e3588dee7cdd2
SnoopyMug <Snoopymug2240@gmail.com>
2025-03-24 22:00:41 -0400

    Looped user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use std::io;
+
+fn main() -> io::Result<()> {
     println!("Hello, world!");
+    let mut input = String::new();
+    loop{
+        println!("Enter a letter");
+        io::stdin().read_line(&mut input)?;
+        match input.trim() {
+            "exit" => break,
+            _ => {// Any input
+                println!("You typed: {}", input.trim());
+            },
+        }
+        input.clear();
+    }
+    Ok(()) // Explicitly needed because of Result
 }
